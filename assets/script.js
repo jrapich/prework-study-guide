5367add7e651540f75e790c6a75086748b051c23
jeremysr <jeremysr@protonmail.com>
2023-06-03 17:44:25 -0600

    added basic javascript functionality and more notes to the HTML file

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+// isnt there a line of code that is supposed to be here at the top of every .js file?
+
+
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+for (let x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+};
+
+/*
+if (topic === "HTML") {
+    console.log("lets study HTML");
+}   else if (topic === "CSS") {
+    console.log("lets study CSS");
+}   else if (topic === "Git") {
+    console.log("lets study Git");
+}   else if (topic === "JavaScript") {
+    console.log("lets study JavaScript");
+}   else {
+    console.log("please try again");
+};
+*/
+
+
+//just loop testing
+//var shapes = ["triangle", "square", "pentagon", "circle"];
+//console.log(`I am logging just the ${shapes[0]}`);
+
+//for (let x = 0; x < shapes.length; x++) {
+//    console.log(shapes[x]);
+//  };
+
