333bf59b54e971f057758b547ab5af66fca9ae48
jeremysr <jeremysr@protonmail.com>
2023-06-03 18:42:21 -0600

    added basic functionality, and added more notes to  the HTML file that I can study

--- assets/script.js
+++ assets/script.js
@@ -2,25 +2,33 @@
 
 
 var topics = ["HTML", "CSS", "Git", "JavaScript"];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for (let x = 0; x < topics.length; x++) {
-    console.log(topics[x]);
+console.log("Here are the topics we learned through PreWork:");
+function listTopics() {
+    for (let x = 0; x < topics.length; x++) {
+        console.log(topics[x]);
+    }
 };
 
-/*
-if (topic === "HTML") {
-    console.log("lets study HTML");
-}   else if (topic === "CSS") {
-    console.log("lets study CSS");
-}   else if (topic === "Git") {
-    console.log("lets study Git");
-}   else if (topic === "JavaScript") {
-    console.log("lets study JavaScript");
-}   else {
-    console.log("please try again");
-};
-*/
+listTopics();
 
+console.log("which topic should we study first?");
+function selectTopic() {
+    if (randomTopic === "HTML") {
+        console.log("lets study HTML");
+    }   else if (randomTopic === "CSS") {
+        console.log("lets study CSS");
+    }   else if (randomTopic === "Git") {
+        console.log("lets study Git");
+    }   else if (randomTopic === "JavaScript") {
+        console.log("lets study JavaScript");
+    }   else {
+        console.log("please try again");
+    };
+};
+
+selectTopic();
 
 //just loop testing
 //var shapes = ["triangle", "square", "pentagon", "circle"];
